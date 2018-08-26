241b6a17b080c9e836c1df7e76d20a97b11e3fef
yasinshaw <yasinshaw@gmail.com>
2018-08-26 19:31:07 +0800

    add interface /hello/github

--- src/main/java/demo/controller/GreetController.java
+++ src/main/java/demo/controller/GreetController.java
@@ -13,4 +13,9 @@ public class GreetController {
             @RequestParam(required = false, defaultValue = "Stranger") String name) {
         return ResponseEntity.ok(String.format("Hello, %s!", name));
     }
+
+    @GetMapping("/hello/github")
+    public ResponseEntity<String> sayHelloGithub() {
+        return ResponseEntity.ok("Hello, I'm github");
+    }
 }
